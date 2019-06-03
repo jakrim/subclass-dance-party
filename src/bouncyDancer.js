38f60e59e7f61ecd8a985d261d46ba1c29d07856
jakrim <jesse.krim@gmail.com>
2019-06-03 17:48:19 -0400

    Implement JoseDancer and add background image

--- src/bouncyDancer.js
+++ src/bouncyDancer.js
@@ -2,6 +2,7 @@ var BouncyDancer = function(top, left, timeBetweenSteps) {
   Dancer.call(this, top, left, timeBetweenSteps);
   this.up = false;
   this.$node.addClass('bouncy');
+  this.bounceSize = 30;
 };
 
 BouncyDancer.prototype = Object.create(Dancer.prototype);
@@ -11,11 +12,11 @@ BouncyDancer.prototype.constructor = BouncyDancer;
 BouncyDancer.prototype.step = function() {
   Dancer.prototype.step.call(this);
   if (!this.up) {
-    this.$node.css({top: this.top - 10, left: this.left});
-    this.top -= 10;
+    this.$node.css({top: this.top - 30, left: this.left});
+    this.top -= 30;
   } else {
-    this.$node.css({top: this.top + 10, left: this.left});
-    this.top += 10;
+    this.$node.css({top: this.top + 30, left: this.left});
+    this.top += 30;
   }
   this.up = !this.up;
 }; 
